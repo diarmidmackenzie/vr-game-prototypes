49269babae7e28098cfc0520fbe87c7ef0954a8a
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-07-31 12:12:34 +0100

    Experiment with physx settings

--- air-hockey/src/spawn-balls.js
+++ air-hockey/src/spawn-balls.js
@@ -10,9 +10,13 @@ AFRAME.registerComponent('spawn-balls', {
         return
       }
 
-      const html = `<a-cylinder id="puck" position="0 1.015 0" radius="0.05" height="0.01" color="white"
+      const html = `<a-cylinder id="puck" position="0 1.005 0" radius="0.05" height="0.01" color="white"
                               physx-body="type: dynamic; highPrecision: true"
-                              physx-material="staticFriction: 0; dynamicFriction: 0; restitution: 1">
+                              physx-material="staticFriction: 0.1; dynamicFriction: 0.1; restitution: 1">
+                      <a-entity physx-joint="type: D6; target:#base; collideWithTarget: false"
+                              physx-joint-constraint__xz="constrainedAxes: x, z; linearLimit: -10 10"
+                              physx-joint-constraint__y="constrainedAxes: y; linearLimit: 0 0">
+                      </a-entity>
                     </a-cylinder>`
       scene.insertAdjacentHTML("beforeend", html)
 
