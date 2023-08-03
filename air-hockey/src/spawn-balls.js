b64cb3bf8831638f47c0818264b812764a9cd15e
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-08-03 11:13:12 +0100

    Solution to restitution issue

--- air-hockey/src/spawn-balls.js
+++ air-hockey/src/spawn-balls.js
@@ -12,7 +12,7 @@ AFRAME.registerComponent('spawn-balls', {
 
       const html = `<a-cylinder id="puck" position="0 1.005 0" radius="0.05" height="0.01" color="white"
                               physx-body="type: dynamic; highPrecision: true"
-                              physx-material="staticFriction: 0.1; dynamicFriction: 0.1; restitution: 1">
+                              physx-material="restitution: 1">
                       <a-entity physx-joint="type: D6; target:#base; collideWithTarget: false"
                               physx-joint-constraint__xz="constrainedAxes: x, z; linearLimit: -10 10"
                               physx-joint-constraint__y="constrainedAxes: y; linearLimit: 0 0">
